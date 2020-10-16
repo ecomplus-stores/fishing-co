c40be52688358dd0688ad4d083589d0344e7c292
Matheus <matheus@e-com.club>
2020-10-15 22:03:33 -0300

    chore: change url

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,1 +1,3 @@
 // Add your custom JavaScript for storefront pages here.
+document.getElementById('c-5f40408bf023684cdbdbe5a1').href = '/ninjanew'
+document.getElementById('c-5f40409af023684cdbdbe5c2').href = '/ninjacamuflada2019'
