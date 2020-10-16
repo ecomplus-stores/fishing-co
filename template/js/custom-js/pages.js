9bb8fdf519f8091788931c57dc35de740385dc99
Matheus <matheus@e-com.club>
2020-10-15 23:38:10 -0300

    chore: update links

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,3 +1,11 @@
 // Add your custom JavaScript for storefront pages here.
 document.getElementById('c-5f40408bf023684cdbdbe5a1').href = '/ninjanew'
 document.getElementById('c-5f40409af023684cdbdbe5c2').href = '/ninjacamuflada2019'
+document.getElementById('c-5f404099f023684cdbdbe5c0').href = '/ziper-masculina'
+document.getElementById('c-5f404097f023684cdbdbe5bc').href = '/basica_new'
+document.getElementById('c-5f40409bf023684cdbdbe5c5').href = '/masculina-recorte'
+document.getElementById('c-5f40408ef023684cdbdbe5a8').href = '/capuz-ninja'
+document.getElementById('c-5f404090f023684cdbdbe5ac').href = '/anorak'
+document.getElementById('c-5f4040a6f023684cdbdbe5dd').href = '/mascara'
+document.getElementById('c-5f40408cf023684cdbdbe5a4').href = '/pernitos'
+document.getElementById('c-5f404094f023684cdbdbe5b5').href = '/porta-latas-robalo'
